144b27ae55528f39671a9a49d8d2835adb3bae20
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-04-23 02:24:59 -0400

    js and txt solution

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,17 +1,34 @@
+
 function consecutiveSubstrings(string) {
-  // type your code here
+
+  const array = []
+  let letters = ""
+
+  for (let i = 0; i <= string.length; i++) {
+
+    for (let j = i + 1; j <= string.length; j++) {
+
+      array.push(string.slice(i, j))
+
+    }
+
+  }
+
+  return array
+
 }
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+console.log(consecutiveSubstrings("Hello"))
 
-  console.log("");
+// if (require.main === module) {
 
-  console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
-}
+//   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
+//   console.log("=>", consecutiveSubstrings('abc'));
+//   console.log("");
+//   console.log("Expecting: ['a']");
+//   console.log("=>", consecutiveSubstrings('a'));
+
+// }
 
 module.exports = consecutiveSubstrings;
 
